303b361843fea08e15115fd521088515e7da382d
Ivan Carlini <ivancarlini99@gmail.com>
2024-04-26 10:19:33 +0200

    bubbles

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from controller import Midi_controller
-from visuals import Squares_and_circles, Image_shift
+from visuals import Squares_and_circles, Image_shift, Bubble, Bubbles
 
 buffer_size = 256
 
@@ -16,24 +16,27 @@ controller = Midi_controller(buffer_size)
 video = cv2.VideoWriter('recordings/output.mov', cv2.VideoWriter_fourcc(*'XVID'), 30, (video_width, video_height))
 
 # choose the visuals you want to draw
-visual = Squares_and_circles(video_width, video_height)
+#visual = Squares_and_circles(video_width, video_height)
 #visual = Image_shift(video_width, video_height)
+#visual = Bubble(video_width, video_height)
+points = []
+for i in range(240, video_width, 240):
+    for j in range(135, video_height, 135):
+        points.append((i, j))
+visual = Bubbles(video_width, video_height, points, 10, 65, (255, 0, 255), 2)
 
 # loop to generate each frame of the video
 while True:
-
     # read MIDI input if there is any
     controller.read()
-    
+
     # create a new empty frame
     frame = np.zeros((video_height, video_width, 3), dtype=np.uint8)
 
     visual.draw_background(frame)
 
     # handle commands
-    for i in range(len(controller.commands)):
-        if controller.commands[i]:
-            visual.draw(frame, [i, controller.velocities[i], controller.colors[i]])
+    visual.draw(frame, [controller.commands, controller.velocities, controller.colors])
 
     # add the frame to the video
     video.write(frame)
--- src/visuals.py
+++ src/visuals.py
@@ -12,7 +12,7 @@ class Squares_and_circles:
         self.p2_square2 = (self.p1_square2[0] + square_size, self.p1_square2[1] + square_size)
         circle_gap = square_size // 6
         circle_margin = 0.8
-        self.circle_ray = int((square_size // 6) * circle_margin)
+        self.circle_radius = int((square_size // 6) * circle_margin)
         p_circle1 = (self.p1_square1[0] + circle_gap, self.p1_square1[1] + circle_gap)
         p_circle2 = (self.p1_square1[0] + circle_gap * 3, self.p1_square1[1] + circle_gap)
         p_circle3 = (self.p1_square1[0] + circle_gap * 5, self.p1_square1[1] + circle_gap)
@@ -39,14 +39,16 @@ class Squares_and_circles:
         cv2.rectangle(frame, self.p1_square1, self.p2_square1, (255, 255, 255), -1)
         cv2.rectangle(frame, self.p1_square2, self.p2_square2, (255, 255, 255), -1)
 
-    # draw a circle by command
+    # draw an image by command data (commands, velocities, colors)
     def draw(self, frame, data):
-        command, velocity, color = data
-        r, g, b = color
-        r = int(r * velocity / 127)
-        g = int(g * velocity / 127)
-        b = int(b * velocity / 127)
-        cv2.circle(frame, self.circles[command % 18], self.circle_ray, (r, g, b), -1)
+        commands, velocities, colors = data
+        for i in range(len(commands)):
+            if commands[i]:
+                r, g, b = colors[i]
+                r = int(r * velocities[i] / 127)
+                g = int(g * velocities[i] / 127)
+                b = int(b * velocities[i] / 127)
+                cv2.circle(frame, self.circles[i % 18], self.circle_radius, (r, g, b), -1)
 
 class Image_shift:
     def __init__(self, video_width, video_height) -> None:
@@ -67,11 +69,79 @@ class Image_shift:
         y_offset = (self.video_height - overlay_h) // 2
         frame[y_offset : y_offset + overlay_h, x_offset : x_offset + overlay_w] = self.background_image[:, :, :3]
 
-    # draw an image by command
+    # draw an image by command data (commands, velocities, colors)
     def draw(self, frame, data):
-        command = data[0]
-        image = self.images[command % len(self.images)]
-        overlay_h, overlay_w, _ = image.shape
-        x_offset = (self.video_width - overlay_w) // 2
-        y_offset = (self.video_height - overlay_h) // 2
-        frame[y_offset : y_offset + overlay_h, x_offset : x_offset + overlay_w] = image[:, :, :3]
+        commands = data[0]
+        for i in range(len(commands)):
+            if commands[i]:
+                image = self.images[i % len(self.images)]
+                overlay_h, overlay_w, _ = image.shape
+                x_offset = (self.video_width - overlay_w) // 2
+                y_offset = (self.video_height - overlay_h) // 2
+                frame[y_offset : y_offset + overlay_h, x_offset : x_offset + overlay_w] = image[:, :, :3]
+
+class Bubble:
+    def __init__(self, video_width, video_height, static_radius = 100, max_radius = 500, static_color = (255, 255, 255), decreasing_velocity = 5) -> None:
+        self.video_width = video_width
+        self.video_height = video_height
+        self.static_radius = static_radius
+        self.static_color = static_color
+        self.radius = static_radius
+        self.max_radius = max_radius
+        self.center = (video_width // 2, video_height // 2)
+        self.decreasing_velocity = decreasing_velocity
+
+    # draw the background image
+    def draw_background(self, frame):
+        cv2.circle(frame, self.center, self.radius, self.static_color, -1)
+        if self.radius > self.static_radius:
+            self.radius -= self.decreasing_velocity
+            self.radius = max(self.radius, self.static_radius)
+                
+    # draw an image by command data (commands, velocities, colors)
+    def draw(self, frame, data):
+        commands = data[0]
+        velocities = data[1]
+        count = 0
+        for i in range(len(commands)):
+            if commands[i]:
+                count += velocities[i] / 127
+        self.radius += int(count * self.decreasing_velocity)
+        self.radius = min(self.radius, self.max_radius)
+
+class Bubbles:
+    def __init__(self, video_width, video_height, bubbles = [], static_radius = 100, max_radius = 500, static_color = (255, 255, 255), decreasing_velocity = 5) -> None:
+        self.video_width = video_width
+        self.video_height = video_height
+        self.static_radius = static_radius
+        self.static_color = static_color
+        self.radius = static_radius
+        self.max_radius = max_radius
+        self.decreasing_velocity = decreasing_velocity
+        self.bubbles = bubbles
+        if not bubbles:
+            p0 = (video_width // 2, video_height // 2)
+            p1 = (video_width // 4, video_height // 4)
+            p2 = (video_width // 4, (video_height * 3) // 4)
+            p3 = ((video_width * 3) // 4, video_height // 4)
+            p4 = ((video_width * 3) // 4, (video_height * 3) // 4)
+            self.bubbles = [p0, p1, p2, p3, p4]
+
+    # draw the background image
+    def draw_background(self, frame):
+        for bubble in self.bubbles:
+            cv2.circle(frame, bubble, self.radius, self.static_color, -1)
+        if self.radius > self.static_radius:
+            self.radius -= self.decreasing_velocity
+            self.radius = max(self.radius, self.static_radius)
+                
+    # draw an image by command data (commands, velocities, colors)
+    def draw(self, frame, data):
+        commands = data[0]
+        velocities = data[1]
+        count = 0
+        for i in range(len(commands)):
+            if commands[i]:
+                count += velocities[i] / 127
+        self.radius += int(count * self.decreasing_velocity)
+        self.radius = min(self.radius, self.max_radius)
